a3acdd724e59bd8ff23867e072b3b582a0feee58
Eliza Canales <elizacanales777@icloud.com>
2024-07-22 16:07:56 -0500

    Realized that a high clip is necessary to remove odd edges

--- programs/VLSR.py
+++ programs/VLSR.py
@@ -7,23 +7,17 @@ def gen_VLSR(path):
     smoothed_data = bm.smooth_data(data=data, smooth=3, polyorder=0)
     rms = bm.estimate_RMS(data=smoothed_data, N=5)
     threshold_mask = bm.get_threshold_mask(data=smoothed_data,
-                                           clip=3.0,
+                                           clip=15.0,
                                            smooth_threshold_mask=0.0)
     masked_data = smoothed_data * threshold_mask
     moments = bm.collapse_first(velax=velax, data=masked_data, rms=rms)
     bm.save_to_FITS(moments=moments, method='first', path=path)
 
 def velocity_range(path):
-    data, velax = bm.load_cube(path)
-    smoothed_data = bm.smooth_data(data=data, smooth=3, polyorder=0)
-    rms = bm.estimate_RMS(data=smoothed_data, N=5)
-    threshold_mask = bm.get_threshold_mask(data=smoothed_data,
-                                           clip=3.0,
-                                           smooth_threshold_mask=0.0)
-    masked_data = smoothed_data * threshold_mask
-    moments = bm.collapse_first(velax=velax, data=masked_data, rms=rms)
-    low = np.nanpercentile(moments[0],5)
-    high = np.nanpercentile(moments[0],95)
+    moments = fits.open(path)[0].data
+    low = np.nanpercentile(moments,5)
+    high = np.nanpercentile(moments,95)
     return(low,high)
 
-print(velocity_range("fits/region1rrl_200.0.fits"))
+gen_VLSR("fits/region1rrl_200.0.fits")
+print(velocity_range("fits/region1rrl_200.0_M1.fits"))
