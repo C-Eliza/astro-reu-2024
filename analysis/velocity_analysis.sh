58884bc286380bd662ecf5a96bded3d98c753417
Eliza Canales <elizacanales777@icloud.com>
2025-08-07 13:57:47 -0600

    Removed analysis scripts

--- analysis/velocity_analysis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Generate simulations with different pixel sizes
-for impix in 50 100 150 200 250 300 350; do
-    python HIIsim.py --impix $impix --fnamebase "test_${impix}"
-done
-
-# Generate velocity statistics
-python VLSR.py fits/test_*rrl_200.0.fits
